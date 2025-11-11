9f6e787e208bc9d82fe763cbdbae5397407e456a
mobiledropbox <localguidephilippines@gmail.com>
2025-11-11 15:26:41 +0800

    Created videoUrl.js.
    Created videoUrl.js.

---
+++ google_chrome_gpu_adjustments/videoUrl.js
@@ -0,0 +1,5 @@
+// Ron Penones | November 11th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+
+export const videoUrl =
+  "https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@main/google_chrome_gpu_adjustments/google_chrome_gpu_adjustments.mp4";
+// ^Iyang nasa taas ang url kung nasaan ang source ng video.
