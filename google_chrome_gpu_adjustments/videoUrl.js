f701afb7d2dc3cb849d59cc79e7971d2cd98f8e9
mobiledropbox <localguidephilippines@gmail.com>
2025-11-11 15:33:44 +0800

    Updated videoUrl.js - line 4.
    Updated videoUrl.js - line 4.

--- google_chrome_gpu_adjustments/videoUrl.js
+++ google_chrome_gpu_adjustments/videoUrl.js
@@ -1,5 +1,5 @@
 // Ron Penones | November 11th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
 
 export const videoUrl =
-  "https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@main/google_chrome_gpu_adjustments/google_chrome_gpu_adjustments.mp4";
+  "https://aeronjegithubdownloads.vercel.app/api/download?url=https%3A%2F%2Fwww.dropbox.com%2Fscl%2Ffi%2F13phm3qwq57bogdpfv609%2Fgoogle_chrome_gpu_adjustments.mp4%3Frlkey%3Ddrsbqosqwaqitqkndyn2q496w%26st%3Dq4kezjtv%26dl%3D1&filename=google_chrome_gpu_adjustments.mp4";
 // ^Iyang nasa taas ang url kung nasaan ang source ng video.
