85017f6f54d2a30fb64b602f627f669e3deeda9d
mobiledropbox <localguidephilippines@gmail.com>
2025-11-12 14:51:34 +0800

    Created icon.js
    Created icon.js

---
+++ assets/icon/icon.js
@@ -0,0 +1,12 @@
+// Ron Penones | November 12th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+
+const cssLink = document.createElement('link');
+cssLink.rel = 'stylesheet';
+cssLink.href = 'https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@main/assets/icon/icon.css';
+document.head.appendChild(cssLink);
+
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.type = 'image/jpg';
+favicon.href = 'https://cdn.jsdelivr.net/gh/mobiledropbox/landing_pages_scripts@main/assets/images/aeronje.jpg';
+document.head.appendChild(favicon);
